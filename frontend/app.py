9ce024c2ac2e3e030bb2ca0902ffe8538dae7aae
aissatou02 <156456598+aissatou02@users.noreply.github.com>
2026-02-05 11:25:57 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 import matplotlib.pyplot as plt
-import os
+
 
 #Là nous sommes sur la configuration
 st.set_page_config(page_title="DataMarché - Interne", layout="wide")
